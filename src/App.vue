6f5f25a74ba95299b7039dc73ae37c0b51aa2bd0
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-29 13:34:44 -0600

    Imported the NewQuote component and created methods to add and delete quotes.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="container">
-        <app-quote-grid :quotes="quotes"></app-quote-grid>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
-    
+    import NewQuote from './components/NewQuote.vue';
     export default {
         data: function(){
             return {
@@ -14,8 +20,17 @@
                 quotes: ['Just a Quote to see something']
             }
         },
+        methods: {
+            newQuote(quote){
+                this.quotes.push(quote);
+            },
+            deleteQuote(index){
+                this.quotes.splice(index, 1);
+            }
+        },
         components: {
-           appQuoteGrid: QuoteGrid
+           appQuoteGrid: QuoteGrid,
+           appNewQuote: NewQuote
         }
     }
 </script>
